a3cf2b2853e20b623794712aa8529f240b96aec2
HunterJiang <15858149091@163.com>
2022-08-23 20:12:38 +0800

    feat: 修改rollup构建的module

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,32 +1,30 @@
-import { defineConfig } from 'rollup'
-import nodeResolve from '@rollup/plugin-node-resolve'
-import typescript from 'rollup-plugin-typescript2';
-import json from '@rollup/plugin-json'
+import { defineConfig } from "rollup";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+import json from "@rollup/plugin-json";
 
 export default defineConfig({
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: {
     dir: "bin",
     format: "commonjs",
     // 添加执行环境
-    banner: '#! /usr/bin/env node'
+    banner: "#! /usr/bin/env node",
   },
   plugins: [
     nodeResolve({
       // 仅作为模块导入
       modulesOnly: true,
-      preferBuiltins: false
+      preferBuiltins: false,
     }),
-    typescript(
-      {
-        tsconfigOverride: {
-          compilerOptions: {
-            // 转为ESNext
-            module: "ESNext"
-          }
-        }
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          // 转为ES2015
+          module: "ES2015",
+        },
       },
-    ),
-    json()
-  ]
-})
+    }),
+    json(),
+  ],
+});
